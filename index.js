1f5acd0a13d6e967dc2e7e776ef2ef7cff4c8860
Sujith P <sujithp2690@gmail.com>
2023-09-29 20:16:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`)
+})
