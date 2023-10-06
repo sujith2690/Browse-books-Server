7dcb97cb9f821a386130e1124cdbc35c701c4300
Sujith P <sujithp2690@gmail.com>
2023-10-06 17:21:31 +0530

    s

--- router/bookRoute.js
+++ router/bookRoute.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import { addNewBook, allNotification, bookCategories, bookSearch, categoryBooks, clearNotification, deleteBook, featuredBooks, getBookDetails, getUserBooks, likeBook, myFavoriteBooks, updateBook } from '../controller/bookController.js';
-import authMiddleWare from '../middleWare/authMiddleware.js';
+import authMiddleware from '../middleWare/authMiddleware.js';
 
 
 const bookRoute = express.Router()
 
-bookRoute.post("/addBook", authMiddleWare, addNewBook)
-bookRoute.post("/updateBook/:id", authMiddleWare, updateBook)
-bookRoute.delete('/deleteBook/:id', authMiddleWare, deleteBook)
-bookRoute.get("/myBook", authMiddleWare, getUserBooks)
-bookRoute.put("/likeBook/:id", authMiddleWare, likeBook)
-bookRoute.get("/myFavoriteBooks", authMiddleWare, myFavoriteBooks)
-bookRoute.get("/notion", authMiddleWare, allNotification)
-bookRoute.patch("/clearNotes", authMiddleWare, clearNotification)
+bookRoute.post("/addBook", authMiddleware, addNewBook)
+bookRoute.post("/updateBook/:id",authMiddleware, updateBook)
+bookRoute.delete('/deleteBook/:id',authMiddleware, deleteBook)
+bookRoute.get("/myBook",authMiddleware, getUserBooks)
+bookRoute.put("/likeBook/:id",authMiddleware, likeBook)
+bookRoute.get("/myFavoriteBooks",authMiddleware, myFavoriteBooks)
+bookRoute.get("/notion",authMiddleware, allNotification)
+bookRoute.patch("/clearNotes",authMiddleware, clearNotification)
 
 bookRoute.get("/featured", featuredBooks)
 bookRoute.get("/categories", bookCategories)
