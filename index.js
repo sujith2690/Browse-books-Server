558bcbb7b4de5958613edee5d454a317026b4a86
Sujith P <sujithp2690@gmail.com>
2023-10-06 16:30:36 +0530

    data

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ import authRoute from './router/authRoute.js';
 import bookRoute from './router/bookRoute.js';
 
 dotenv.config();
-const CLIENT = process.env.CLIENT
+
 const app = express()
-app.use(cors({ origin: [CLIENT, "http://localhost:3000"] }))
+app.use(cors())
 app.use(express.json({ limit: '20mb' }));
 app.use(bodyParser.json({ extended: true, limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: true }))
--- router/bookRoute.js
+++ router/bookRoute.js
@@ -5,21 +5,21 @@ import authMiddleWare from '../middleWare/authMiddleware.js';
 
 const bookRoute = express.Router()
 
-bookRoute.post("/addBook",authMiddleWare,addNewBook)
-bookRoute.post("/updateBook/:id",authMiddleWare,updateBook)
-bookRoute.delete('/deleteBook/:id',authMiddleWare,deleteBook)
-bookRoute.get("/myBook",authMiddleWare,getUserBooks)
-bookRoute.put("/likeBook/:id",authMiddleWare,likeBook)
-bookRoute.get("/myFavoriteBooks",authMiddleWare,myFavoriteBooks)
-bookRoute.get("/notion",authMiddleWare,allNotification)
-bookRoute.patch("/clearNotes",authMiddleWare,clearNotification)
+bookRoute.post("/addBook", authMiddleWare, addNewBook)
+bookRoute.post("/updateBook/:id", authMiddleWare, updateBook)
+bookRoute.delete('/deleteBook/:id', authMiddleWare, deleteBook)
+bookRoute.get("/myBook", authMiddleWare, getUserBooks)
+bookRoute.put("/likeBook/:id", authMiddleWare, likeBook)
+bookRoute.get("/myFavoriteBooks", authMiddleWare, myFavoriteBooks)
+bookRoute.get("/notion", authMiddleWare, allNotification)
+bookRoute.patch("/clearNotes", authMiddleWare, clearNotification)
 
-bookRoute.get("/featured",featuredBooks)
-bookRoute.get("/categories",bookCategories)
-bookRoute.get("/categoryBooks/:categoryName",categoryBooks)
+bookRoute.get("/featured", featuredBooks)
+bookRoute.get("/categories", bookCategories)
+bookRoute.get("/categoryBooks/:categoryName", categoryBooks)
 
-bookRoute.get("/search",bookSearch)
-bookRoute.get("/:id",getBookDetails)
+bookRoute.get("/search", bookSearch)
+bookRoute.get("/:id", getBookDetails)
 
 
 
